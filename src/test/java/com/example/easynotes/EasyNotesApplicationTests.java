25ed697d74bb8a1eceb6a3171ef0fbd14839af88
unknown <luccasfabio@gmail.com>
2020-01-21 23:38:18 -0300

    Commit inicial

---
+++ src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.easynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
